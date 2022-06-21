9a86fc7fcd7bd9315e88ab63fcdc38a629de36d2
Hermanda Mak <hermandamak@gmail.com>
2022-06-20 22:19:09 -0700

    moved state and context to Wrapper

--- WEEK3/navbar-context/src/App.js
+++ WEEK3/navbar-context/src/App.js
@@ -2,19 +2,15 @@ import './App.css';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
 import FormWrapper from './components/FormWrapper';
-import {useState} from 'react';
-import NameContext from './context/NameContext';
+
 
 function App() {
 
-  const [name, setName] = useState("user")
 
   return (
   <Wrapper>
-    <NameContext.Provider value={{name, setName}}>
-      <Navbar />
-      <FormWrapper />
-    </NameContext.Provider>
+    <Navbar />
+    <FormWrapper />
   </Wrapper>
   );
 }
--- WEEK3/navbar-context/src/components/Wrapper.jsx
+++ WEEK3/navbar-context/src/components/Wrapper.jsx
@@ -1,7 +1,15 @@
+import {useState} from 'react';
+import NameContext from '../context/NameContext';
+
 const Wrapper = (props) => {
+
+  const [name, setName] = useState("user")
+
   return (
     <div className="container-fluid" >
-      {props.children}
+      <NameContext.Provider value={{name, setName}}>
+        {props.children}
+      </NameContext.Provider>
     </div>
   );
 }
