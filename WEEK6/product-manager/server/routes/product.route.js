6cef039155518b62eae798f780a00323072307fc
Hermanda Mak <hermandamak@gmail.com>
2022-07-13 17:21:27 -0700

    updated route for put and delete

--- WEEK6/product-manager/server/routes/product.route.js
+++ WEEK6/product-manager/server/routes/product.route.js
@@ -4,6 +4,6 @@ module.exports = app => {
   app.get('/api/product', ProductController.findAllProduct);
   app.get('/api/product/:id', ProductController.findOneProduct);
   app.post('/api/product', ProductController.createProduct);
-  app.put('/api/:id', ProductController.updateProduct);
-  app.delete('/api/:id', ProductController.deleteProduct);
+  app.put('/api/product/:id', ProductController.updateProduct);
+  app.delete('/api/product/:id', ProductController.deleteProduct);
 }
