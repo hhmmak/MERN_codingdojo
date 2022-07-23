56f35128e5f77549183ad143beaf623f47dea4ee
Hermanda Mak <hermandamak@gmail.com>
2022-07-23 14:52:50 -0700

    add birthday button

--- WEEK8/big-inversion/src/components/PersonCard.js
+++ WEEK8/big-inversion/src/components/PersonCard.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
+// import BirthdayButton from './BirthdayButton';
 
 class PersonCard extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.age
+    };
+  }
   
   render() {
-
-    const { firstName, lastName, age, hairColor} = this.props;
     
     return (
       <div className='card'>
-        <h3>{lastName}, {firstName}</h3>
-        <p>Age: {age}</p>
-        <p>Hair Color: {hairColor}</p>
+        <h3>{this.props.lastName}, {this.props.firstName}</h3>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+        <button onClick={() => this.setState({age: this.state.age + 1})} className={"bdayBtn"}>Birthday Button for {this.props.firstName + " " + this.props.lastName}</button>
       </div>
     )
   }
