94d0fc21b1b11c1752c7f54b0f183d3ba8b51598
Hermanda Mak <hermandamak@gmail.com>
2022-07-23 14:15:05 -0700

    initial commit

---
+++ WEEK8/big-inversion/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+
+  const people = [
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+      age: 19,
+      hairColor: "brown"
+    },
+    {
+      firstName: "Jack",
+      lastName: "Chen",
+      age: 12,
+      hairColor: "grey"
+    },
+    {
+      firstName: "Sam",
+      lastName: "Cho",
+      age: 23,
+      hairColor: "black"
+    },
+  ]
+
+
+  return (
+    <div className="container">
+      { people.map( person =>
+          <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ WEEK8/big-inversion/src/components/PersonCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PersonCard extends Component {
+
+  
+  render() {
+
+    const { firstName, lastName, age, hairColor} = this.props;
+    
+    return (
+      <div className='card'>
+        <h3>{lastName}, {firstName}</h3>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hairColor}</p>
+      </div>
+    )
+  }
+}
+
+export default PersonCard
